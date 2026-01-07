1ffe6f5f303cf8ae9ae5fa9186e4cc6f71b94567
YerB-QZ <zhumabaye@gmail.com>
2026-01-07 20:34:44 +0300

    Navigate to reserve table added

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,14 @@
 import "./Main.css"
 import restauranfood from "../images/restauranfood.jpg";
+import { useNavigate } from "react-router-dom";
+
+
 
 function Homepage() {
-
+const navigate = useNavigate();
+const handleReserve = () => {
+  navigate("/booking");
+};
 return (
 <section className="Main">
 <section className="homepage">
@@ -10,7 +16,7 @@ return (
 <h1>Little Lemon</h1>
 <h2>Chicago</h2>
 <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-<button>Reserve a Table</button>
+<button onClick={handleReserve}>Reserve a Table</button>
 </div>
 <div className="homeImage">
 <img src = {restauranfood} alt = "Mediterranean restaurant food"/>
